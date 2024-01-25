b59adc4413a59f3f58eff85fa721597117bc3284
AndreMaia95 <andrefsm95@gmail.com>
2024-01-25 20:07:58 +0000

    feat add: game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,156 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const countries = [
+    "portugal",
+    "brazil",
+    "spain",
+    "france",
+    "germany",
+    "italy",
+    "england",
+    "ireland",
+    "norway",
+    "sweden",
+    "finland",
+    "denmark",
+    "iceland",
+    "greece",
+    "turkey",
+    "russia",
+    "poland",
+    "slovakia",
+    "austria",
+    "switzerland",
+    "netherlands",
+    "belgium",
+    "luxembourg",
+    "liechtenstein",
+    "andorra",
+    "monaco",
+    "san marino",
+    "vatican",
+    "malta",
+    "albania",
+    "macedonia",
+    "montenegro",
+    "serbia",
+    "croatia",
+    "slovenia",
+    "hungary",
+    "romania",
+    "bulgaria",
+    "moldova",
+    "ukraine",
+    "belarus",
+    "lithuania",
+    "latvia",
+    "estonia",
+    "armenia",
+    "georgia",
+    "azerbaijan",
+    "kazakhstan",
+    "turkmenistan",
+    "uzbekistan",
+    "tajikistan",
+    "kyrgyzstan",
+    "afghanistan",
+    "pakistan",
+    "india",
+    "nepal",
+    "bhutan",
+    "bangladesh",
+  ];
+
+  const [randomCountry, setRandomCountry] = useState<string>("");
+  const [revealedLetters, setRevealedLetters] = useState<boolean[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  //Escolher um país aleatório que esteja no array
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    setRandomCountry(countries[randomIndex]);
+  }, []);
+
+  //atualiza os quadrados revelados sempre que o país aleatório muda
+  useEffect(() => {
+    setRevealedLetters(new Array(randomCountry.length).fill(false));
+  }, [randomCountry]);
+
+  const handleButtonClick = () => {
+    const inputValueLowerCase = inputValue.toLowerCase();
+
+    // Verifica se a entrada do usuário contém alguma letra da palavra aleatória
+    const containsMatchingLetters = inputValueLowerCase
+      .split("")
+      .some((letter) => randomCountry.includes(letter));
+
+    if (containsMatchingLetters) {
+      // Se a entrada contiver letras coincidentes, revele essas letras nos quadrados
+      const newRevealedLetters: boolean[] = revealedLetters.map((_, index) =>
+        inputValueLowerCase.includes(randomCountry[index])
+          ? true
+          : revealedLetters[index]
+      );
+      setRevealedLetters(newRevealedLetters);
+    }
+    if (inputValue === randomCountry) {
+      setRevealedLetters(new Array(randomCountry.length).fill(true));
+      setTimeout(() => {
+        openModal();
+      }, 300);
+    }
+
+    // Limpa o campo de entrada
+    setInputValue("");
+  };
+
+  //Quando botão enter é clicado efetua o click no botao OK
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleButtonClick();
+    }
+  };
+
+  const openModal = () => {
+    alert("Parabéns, você acertou!");
+    window.location.reload();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-screen w-full bg-slate-500 flex items-center justify-center">
+      <div className="flex flex-col h-screen">
+        <div className="h-[85%] flex flex-col items-center justify-center">
+          <div className="absolute top-10">
+            <h1 className="text-center font-semibold text-4xl">MindWhiz</h1>
+            <p className="text-center font-normal">Try to find the country!</p>
+          </div>
+          <div className="flex gap-1">
+            {randomCountry.split("").map((letter, index) => (
+              <div
+                key={index}
+                className=" w-8 h-8 bg-white rounded-md flex items-center justify-center text-xl font-bold sm:w-10 sm:h-10 sm:text-2xl lg:w-16 lg:h-16 lg:text-5xl"
+              >
+                {revealedLetters[index] ? letter.toUpperCase() : null}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-1 items-center justify-center">
+          <input
+            value={inputValue}
+            onKeyDown={handleKeyDown}
+            onChange={(event) => setInputValue(event.target.value)}
+            className="w-[300px] rounded-tl-md rounded-bl-md px-5 py-4 font-[20px]"
+          ></input>
+          <button
+            onClick={handleButtonClick}
+            className="px-5 py-4 bg-white border-l border-zinc-500 rounded-tr-md rounded-br-md hover:bg-slate-300"
+          >
+            OK
+          </button>
+        </div>
       </div>
     </main>
   );
